0ef1fd9161fdb360cbd09c00aabd95ea0b147f97
Quarmby-Wilds <cedar.quarmby@bellevuecollege.edu>
2025-11-15 19:24:33 -0800

    Add content for login page

--- js/index.js
+++ js/index.js
@@ -1,42 +1,35 @@
-////---- Search ----////
+//// ---- SEARCH ---- ////
 
 // - Search on index.html - //
 
-function search(){
-    const inputValue = document.getElementById("search").value 
-    const searchResult = document.getElementById("search-result"); 
+function search() {
+    const inputValue = document.getElementById("search").value;
+    const searchResult = document.getElementById("search-result");
 
-    if (inputValue.toUpperCase() === "BELLEVUE COLLEGE"){
-        searchResult.innerHTML = 'Results: <br>';
+    if (inputValue.toUpperCase() === "BELLEVUE COLLEGE") {
+        searchResult.innerHTML = "Results: <br>";
         searchResult.innerHTML += '<a href="html/analysis.html">Bellevue College Analysis</a> <br>';
         searchResult.innerHTML += '<a href="html/dashboard.html">Bellevue College Dashboard</a> <br>';
         searchResult.innerHTML += '<a href="html/summary.html">Bellevue College Summary</a> <br>';
-    }
-    else if (inputValue.toUpperCase() === "BELLEVUE COLLEGE "){
-        searchResult.innerHTML = 'Results: <br>';
+    } else if (inputValue.toUpperCase() === "BELLEVUE COLLEGE ") {
+        searchResult.innerHTML = "Results: <br>";
         searchResult.innerHTML += '<a href="html/analysis.html">Bellevue College Analysis</a> <br>';
         searchResult.innerHTML += '<a href="html/summary.html">Bellevue College Summary</a> <br>';
-    }
-    else if(inputValue.toUpperCase() === "BELLEVUE COLLEGE ANALYSIS"){
-        searchResult.innerHTML = 'Results <br>';
+    } else if (inputValue.toUpperCase() === "BELLEVUE COLLEGE ANALYSIS") {
+        searchResult.innerHTML = "Results <br>";
         searchResult.innerHTML += '<a href="html/analysis.html">Bellevue College Analysis</a> <br>';
-    }
-    else if(inputValue.toUpperCase() === "BELLEVUE COLLEGE SUMMARY"){
-        searchResult.innerHTML = 'Results: <br>';
+    } else if (inputValue.toUpperCase() === "BELLEVUE COLLEGE SUMMARY") {
+        searchResult.innerHTML = "Results: <br>";
         searchResult.innerHTML += '<a href="html/summary.html">Bellevue College Summary</a> <br>';
-    }
-    else if(inputValue.toUpperCase() === "BELLEVUE COLLEGE DASHBOARD"){
-        searchResult.innerHTML = 'Results: <br>';
+    } else if (inputValue.toUpperCase() === "BELLEVUE COLLEGE DASHBOARD") {
+        searchResult.innerHTML = "Results: <br>";
         searchResult.innerHTML += '<a href="html/dashboard.html">Bellevue College Dashboard</a> <br>';
-    }
-    else if (inputValue.toUpperCase() === ""){
-        searchResult.innerHTML = '';
-        searchResult.innerHTML += 'Which college are you looking for?';
-    }
-    else{
-        searchResult.innerHTML = 'Results: <br>';
-        searchResult.innerHTML += 'The search that you are looking for is not found';
-
+    } else if (inputValue.toUpperCase() === "") {
+        searchResult.innerHTML = "";
+        searchResult.innerHTML += "Which college are you looking for?";
+    } else {
+        searchResult.innerHTML = "Results: <br>";
+        searchResult.innerHTML += "The search that you are looking for is not found";
     }
 }
 
@@ -64,3 +57,42 @@ window.onclick = function (event) {
         closeModal();
     }
 };
+
+//// ---- SIGNUP ---- ////
+
+function showSection(section) {
+    const sections = document.querySelectorAll(".collection-section");
+    sections.forEach((s) => (s.style.display = "none"));
+    document.getElementById(section).style.display = "block";
+
+    const buttons = document.querySelectorAll('p[align="center"] button');
+    buttons.forEach((b) => b.classList.remove("active"));
+    event.target.classList.add("active");
+}
+
+const signupButton = document.getElementById("signupBtn");
+const signupForm = document.getElementById("signupForm");
+const resultsList = document.getElementById("results");
+
+signupForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(signupForm);
+    const data = {};
+
+    resultsList.innerHTML = "";
+
+    formData.forEach((value, name) => {
+        data[name] = value;
+        resultsList.append(`${name}: ${value}`);
+        resultsList.append(document.createElement("br"));
+    });
+
+    localStorage.setItem("signupData", JSON.stringify(data));
+
+    document.getElementById("signup").style.display = "none";
+
+    signupButton.style.display = "none";
+
+    // document.getElementById("resultsPage").style.display = "block";
+});
